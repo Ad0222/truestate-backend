64102570c2c7b87af67d9e058ef69cc8c89a8009
Amit Nagar <dhakadamit53@gmail.com>
2025-12-14 15:37:03 +0530

    Added CSV data and updated csv loader

--- src/utils/csvLoader.js
+++ src/utils/csvLoader.js
@@ -1,47 +1,33 @@
+const fs = require("fs");
 const csv = require("csv-parser");
-const fetch = require("node-fetch");
-const { Readable } = require("stream");
 
-const CSV_URL =
-  "https://drive.google.com/file/d/1WpgRyEN3N5A8-8nUohVoZNkhhVhEocds/view?usp=drive_link";
+const CSV_PATH = "./data/truestate_assignment_datasets.csv";
 
-// Convert CSV text into stream for csv-parser
-function textToStream(text) {
-  return Readable.from(text);
-}
-
-async function fetchCSVFromURL() {
-  try {
-    const response = await fetch(CSV_URL);
-    const csvText = await response.text();
-
+async function loadCSV() {
+  return new Promise((resolve, reject) => {
     const results = [];
 
-    return new Promise((resolve, reject) => {
-      textToStream(csvText)
-        .pipe(csv())
-        .on("data", (row) => results.push(row))
-        .on("end", () => {
-          console.log("✔ CSV Loaded from Google Drive:", results.length, "rows");
-          resolve(results);
-        })
-        .on("error", (err) => reject(err));
-    });
-  } catch (error) {
-    console.error("❌ Error fetching CSV:", error);
-    return [];
-  }
+    fs.createReadStream(CSV_PATH)
+      .pipe(csv())
+      .on("data", (row) => results.push(row))
+      .on("end", () => {
+        console.log(`✔ Local CSV Loaded: ${results.length} rows`);
+        resolve(results);
+      })
+      .on("error", (err) => reject(err));
+  });
 }
 
 async function searchCSV(keyword) {
   const searchValue = keyword.toLowerCase();
-  const allData = await fetchCSVFromURL();
+
+  const allData = await loadCSV();
 
   const filtered = allData.filter((row) =>
     Object.values(row).join(" ").toLowerCase().includes(searchValue)
   );
 
-  console.log(`🔍 Search: "${keyword}" → ${filtered.length} result(s)`);
+  console.log(`🔍 Search: "${keyword}" → ${filtered.length} results`);
 
   return filtered;
 }
